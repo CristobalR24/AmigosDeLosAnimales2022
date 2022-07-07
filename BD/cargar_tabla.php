e3be30aaa2a0369fbd415d7b6d4c96881e5365e4
auziel27 <86246517+auziel27@users.noreply.github.com>
2022-07-07 17:25:42 -0500

    Nuevo

---
+++ BD/cargar_tabla.php
@@ -0,0 +1,34 @@
+<?php
+
+class obtener
+{
+    function solicitudes()
+    {include("conexion.php");
+        $sql="call Obtener_Solicitud(?)";
+        $valor=3;
+        $consulta=$conn->prepare($sql);
+        $consulta->bindParam(1,$valor,PDO::PARAM_STR);
+        $consulta->execute();
+        
+        while($row=$consulta->fetch())
+        {
+        $ID=$row["Id_soli"];
+        $mascota=$row["Nombre"];
+        $Tipo=$row["Tipo_Formulario"];
+        $nombre_adop=$row["Nombre_Adop"];
+        $fecha=$row["Fecha"];
+        echo "<tr>";
+        echo "<td>".$ID."</td>";
+        echo "<td>".$mascota."</td>";
+        echo "<td>".$Tipo."</td>";
+        echo "<td>".$nombre_adop."</td>";
+        echo "<td>".$fecha."</td>";
+        echo "<td><button class='btn btn-primary' id='in' onclick='testeo()'>Ver información</button></td>";
+            // <td><button class='btn btn-primary' id="in" onclick="test()">Ver información</button></td>
+               
+
+        }
+    }
+}
+
+?>
